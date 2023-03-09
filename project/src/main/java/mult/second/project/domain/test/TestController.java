fe869946709115f821e73b56ec80f43977f57fca
joonhyung <junjin9988@gmail.com>
2023-03-09 16:37:06 +0900

    Revert "Merge branch 'joonhyung22' of https://github.com/sieunnnn/MultiCampusProject into joonhyung22"
    This reverts commit 615a8eb3ff82cc9270780888868fb9f5eee591fd, reversing
    changes made to 1596aa140f5ef8f63fa5cd9e69f5ce619f9c8a2f.

--- project/src/main/java/mult/second/project/domain/test/TestController.java
+++ project/src/main/java/mult/second/project/domain/test/TestController.java
@@ -7,11 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-//    @GetMapping(value = "/")
-//    public String layout() {
-//        return "main_layout";
-//    }
-
     @GetMapping(value = "/")
     public String test() {
         return "test";
