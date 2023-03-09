7225d749999b983deff890d71754762351b7991d
sieunnnn <wldsmtldsm65@gmail.com>
2023-03-09 18:36:10 +0900

    modify TestController for test

--- project/src/main/java/multi/second/project/domain/test/TestController.java
+++ project/src/main/java/multi/second/project/domain/test/TestController.java
@@ -7,27 +7,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-//    @GetMapping(value = "/")
-//    public String layout() {
-//        return "main_layout";
-//    }
+    @GetMapping(value = "/")
+    public String layout() {
+        return "main_layout";
+    }
 
 //    @GetMapping(value = "/")
 //    public String test() {
 //        return "test";
 //    }
 
-//    @GetMapping(value = "/")
-//    public String member() {
-//        return "member1/login";
-//    }
-//
-//    @GetMapping(value = "/")
-//    public String Profile() {
-//        return "profile1/profile";
-//   }
-//
-    @GetMapping(value = "/")
+    @GetMapping(value = "/login")
+    public String member() {
+        return "member1/login";
+    }
+
+    @GetMapping(value = "/profile")
+    public String Profile() {
+        return "profile1/profile";
+   }
+
+    @GetMapping(value = "/friends")
     public String friends() {
         return "messages1/friends";
     }
