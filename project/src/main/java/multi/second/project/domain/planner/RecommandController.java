8bb19b1599731541017f14f3a00a462f6bac4d47
bjh <workbaejh@gmail.com>
2023-03-22 01:59:18 +0900

    websocket 채팅기능 구현-bjh

--- project/src/main/java/multi/second/project/domain/planner/RecommandController.java
+++ project/src/main/java/multi/second/project/domain/planner/RecommandController.java
@@ -1,14 +1,18 @@
 package multi.second.project.domain.planner;
 
+import java.util.Map;
+
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
-import multi.second.project.domain.planner.dto.PlanRecommandRequest;
+import multi.second.project.domain.member.UserPrincipal;
+import multi.second.project.domain.planner.dto.ChatRequest;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,10 +29,22 @@ public class RecommandController {
 		 return "recommand/recommand";
 	 }
 	
+//	  @MessageMapping("/recommand-plan/{tgIdx}")
+//	  public void recommandPlan(@DestinationVariable("tgIdx") Long tgIdx, PlanRecommandRequest dto) throws Exception {
+//	      simpMessagingTemplate.convertAndSend("/topic/recommand-plan/" + tgIdx, recommandService.generateTravelPlan(dto));
+//	  }
+//	  @MessageMapping("/recommand-plan/{tgIdx}")
+//	  public void recommandPlan(@DestinationVariable("tgIdx") Long tgIdx, ChatRequest dto) throws Exception {
+//		  simpMessagingTemplate.convertAndSend("/topic/recommand-plan/" + tgIdx, recommandService.generateChat(dto));
+//	  }
 	  @MessageMapping("/recommand-plan/{tgIdx}")
-	  public void recommandPlan(@DestinationVariable("tgIdx") Long tgIdx, PlanRecommandRequest dto) throws Exception {
-	      simpMessagingTemplate.convertAndSend("/topic/recommand-plan/" + tgIdx, recommandService.generateTravelPlan(dto));
+	  public void recommandPlan(@DestinationVariable("tgIdx") Long tgIdx, ChatRequest dto) throws Exception {
+		  simpMessagingTemplate.convertAndSend("/topic/recommand-plan/" + tgIdx, recommandService.generateChat(dto));
 	  }
+//	  @MessageMapping("/recommand-plan/{tgIdx}")
+//	  public void recommandPlan(@DestinationVariable("tgIdx") Long tgIdx, @RequestParam Map<String, String> message) throws Exception {
+//		  return Map.of("message" ,"["+ UserPrincipal.getUserPrincipal().getUserId() +"]"+" : " );
+//	  }
 	
 	  
 	  
