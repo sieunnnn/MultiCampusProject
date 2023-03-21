f0f12b9af61ad4fba2ce428be489389f07ea0b93
sieunnnn <wldsmtldsm65@gmail.com>
2023-03-21 22:10:51 +0900

    실시간 채팅 | 최근 버전 초안

--- project/src/main/java/multi/second/project/domain/chat/ChatController.java
+++ project/src/main/java/multi/second/project/domain/chat/ChatController.java
@@ -1,17 +1,36 @@
 package multi.second.project.domain.chat;
 
+import multi.second.project.domain.member.domain.Member;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class ChatController {
 
     @GetMapping("/chat")
-    public String chatGET(HttpSession session) {
-        System.out.println("@ChatController, chat GET()");
-        return "chat";
+    public String chatGet() {
+        return "index";
+    }
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
     }
 
 }
--- project/src/main/java/multi/second/project/infra/config/WebSocketConfig.java
+++ project/src/main/java/multi/second/project/infra/config/WebSocketConfig.java
@@ -18,7 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    registry.addEndpoint("/ws/chat").withSockJS();
   }
-
 }
