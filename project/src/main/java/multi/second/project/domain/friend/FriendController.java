eead45519564a3524eab9163a0261f724439a67c
Joongjii <guswnddl1217@gmail.com>
2023-03-30 22:15:23 +0900

    .

--- project/src/main/java/multi/second/project/domain/friend/FriendController.java
+++ project/src/main/java/multi/second/project/domain/friend/FriendController.java
@@ -1,62 +1,58 @@
 package multi.second.project.domain.friend;
 
+import lombok.AllArgsConstructor;
 import multi.second.project.domain.friend.domain.Friend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @Controller
+@AllArgsConstructor
+@RequestMapping("firends")
 public class FriendController {
+
     private final FriendService friendService;
 
-    @Autowired
-    public FriendController(FriendService friendService) {
-        this.friendService = friendService;
-    }
-
-    @GetMapping("friends")
-    public String home(Model model) {
+    @GetMapping
+    public String getFriends(Model model) {
         List<Friend> friends = friendService.getAllFriends();
         model.addAttribute("friends", friends);
-        return "index";
+        return "friends/friends";
     }
 
-    @GetMapping("/add/friend")
+    @GetMapping("/add")
     public String addFriendForm(Model model) {
         model.addAttribute("friend", new Friend());
-        return "addFriendForm";
+        return "friends/addFriendForm";
     }
 
-    @PostMapping("/add/Friend")
+    @PostMapping("/add")
     public String addFriendSubmit(@ModelAttribute Friend friend) {
         friendService.saveFriend(friend);
-        return "redirect:/";
+        return "redirect:/friends";
     }
 
-    @GetMapping("/editFriend/{id}")
-    public String editFriendForm(@PathVariable Long id, Model model) {
-        Optional<Friend> friend = friendService.getFriendById(id);
-        model.addAttribute("friend", friend.orElseThrow(() -> new IllegalArgumentException("Invalid friend id:" + id)));
-        return "editFriendForm";
-    }
+//    @GetMapping("/edit/{id}")
+//    public String editFriendForm(@PathVariable Long id, Model model) {
+//        Optional<Friend> friend = friendService.getFriendById(id);
+//        model.addAttribute("friend", friend.orElseThrow(() -> new IllegalArgumentException("Invalid friend id:" + id)));
+//        return "friends/editFriendForm";
+//    }
+//
+//    @PostMapping("/edit/{id}")
+//    public String editFriendSubmit(@PathVariable Long id, @ModelAttribute Friend friend) {
+//        friend.setId(id);
+//        friendService.saveFriend(friend);
+//        return "redirect:/friends";
+//    }
 
-    @PostMapping("/editFriend/{id}")
-    public String editFriendSubmit(@PathVariable Long id, @ModelAttribute Friend friend) {
-       // friend.setId(id);
-        friendService.saveFriend(friend);
-        return "redirect:/";
-    }
-
-    @GetMapping("/delete/Friend/{id}")
+    @GetMapping("/delete/{id}")
     public String deleteFriend(@PathVariable Long id) {
         friendService.deleteFriendById(id);
-        return "redirect:/";
+        return "redirect:/friends";
     }
 }
