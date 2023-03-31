ae56dae774cfab6f343f8fcec432e64ebc07984a
bjh <workbaejh@gmail.com>
2023-04-01 00:38:36 +0900

    투두리스트 connect 하나로 합치기

--- project/src/main/java/multi/second/project/domain/todolist/TodoListController.java
+++ project/src/main/java/multi/second/project/domain/todolist/TodoListController.java
@@ -46,80 +46,69 @@ public class TodoListController {
 	private final TodoListService todoListService;
 	private final SimpMessagingTemplate simpMessagingTemplate;
 
-//	@PostMapping("upload")
-//	public String upload(
-//			TodoListRegistRequest dto,
-//			Long tpIdx
-//			) {
-//		
-//		todoListService.createTodoList(dto, tpIdx);
-//		
-//		return "redirect:/planner/detail?tpIdx="+tpIdx;
-//	}
-	
 	@MessageMapping("/upload-todolist/{tpIdx}")
 	public void upload(
 			@DestinationVariable("tpIdx") Long tpIdx,
 			TodoListRegistRequest dto
 			) throws Exception {
-		simpMessagingTemplate.convertAndSend("/topic/upload-todolist/" + tpIdx, todoListService.createTodoList(dto, tpIdx));
+		simpMessagingTemplate.convertAndSend("/topic/planner-message/" + tpIdx, 
+				Map.of("type","upload-todolist","msg",todoListService.createTodoList(dto, tpIdx)));
 		
 	}
-	
-//	@PostMapping("modify")
-//	public String modify(
-//			TodoListModifyRequest dto,
-//			Long tpIdx
-//			) {
-//
-//		todoListService.updateTodoList(dto);
-//		
-//		return "redirect:/planner/detail?tpIdx="+tpIdx;
-//	}
-	
-	//삭제나 업데이트는 어떻게...?
-	//삭제는 리퀘스트에 isdel 바꾸는거 넣어서?
-	
+
 	@MessageMapping("modify-todolist/{tpIdx}")
 	public void modify(
 			@DestinationVariable("tpIdx") Long tpIdx,
 			TodoListModifyRequest dto
 			) throws Exception {
 
-		simpMessagingTemplate.convertAndSend("/topic/modify-todolist/" + tpIdx, todoListService.updateTodoList(dto));
+		simpMessagingTemplate.convertAndSend("/topic/planner-message/" + tpIdx,
+				Map.of("type","modify-todolist","msg",todoListService.updateTodoList(dto)));
 		
 	}
-	
-//	@PostMapping("remove")
-//	public String remove(
-//			Long tlIdx, 
-//			Long tpIdx
-//			) {
-//		
-//		todoListService.deleteTodoList(tlIdx, tpIdx);
-//		return "redirect:/gallery/detail?postIdx="+tpIdx;
-//	}
-	
+
 	@MessageMapping("remove-todolist/{tpIdx}")
-	@SendTo("/topic/remove-todolist/{tpIdx}")
-	public Map<String,Long> remove( 
+	public void remove( 
 			@DestinationVariable("tpIdx") Long tpIdx,
 			TodoListDeleteRequest dto
 			) throws Exception {
 		todoListService.deleteTodoList(dto.getTlIdx(), tpIdx);
 		
-		return Map.of("message",dto.getTlIdx());
-		
+		simpMessagingTemplate.convertAndSend("/topic/planner-message/" + tpIdx,
+				Map.of("type","remove-todolist","msg",dto.getTlIdx()));
 	}
-//	@MessageMapping("remove-todolist/{tpIdx}")
-//	public void remove( 
+	
+//	@MessageMapping("/upload-todolist/{tpIdx}")
+//	public void upload(
 //			@DestinationVariable("tpIdx") Long tpIdx,
-//			Long tlIdx
-//			) {
-//		simpMessagingTemplate.convertAndSend("/topic/remove-todolist/" + tpIdx, todoListService.deleteTodoList(tlIdx, tpIdx));
+//			TodoListRegistRequest dto
+//			) throws Exception {
+//		simpMessagingTemplate.convertAndSend("/topic/upload-todolist/" + tpIdx, todoListService.createTodoList(dto, tpIdx));
 //		
 //	}
-	
+//	
+//	@MessageMapping("modify-todolist/{tpIdx}")
+//	public void modify(
+//			@DestinationVariable("tpIdx") Long tpIdx,
+//			TodoListModifyRequest dto
+//			) throws Exception {
+//		
+//		simpMessagingTemplate.convertAndSend("/topic/modify-todolist/" + tpIdx, todoListService.updateTodoList(dto));
+//		
+//	}
+//	
+//	@MessageMapping("remove-todolist/{tpIdx}")
+//	@SendTo("/topic/remove-todolist/{tpIdx}")
+//	public Map<String,Long> remove( 
+//			@DestinationVariable("tpIdx") Long tpIdx,
+//			TodoListDeleteRequest dto
+//			) throws Exception {
+//		todoListService.deleteTodoList(dto.getTlIdx(), tpIdx);
+//		
+//		return Map.of("message",dto.getTlIdx());
+//		
+//	}
+
 	
 	
 	
