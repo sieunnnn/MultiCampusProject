532082cf66bd734e5c494c117e8f5db44a4b1f4a
Joongjii <guswnddl1217@gmail.com>
2023-04-02 23:01:15 +0900

    작업중

---
+++ project/src/main/java/multi/second/project/domain/note/dto/response/NoteListResponse.java
@@ -0,0 +1,12 @@
+package multi.second.project.domain.note.dto.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class NoteListResponse {
+
+    private Long ntIdx;
+
+}
