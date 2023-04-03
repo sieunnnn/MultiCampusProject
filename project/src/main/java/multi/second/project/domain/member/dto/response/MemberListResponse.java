7e95ab55f83abb340430f24bf19aaa44747fdc4f
Joongjii <guswnddl1217@gmail.com>
2023-04-03 12:59:36 +0900

    작업중

--- project/src/main/java/multi/second/project/domain/member/dto/response/MemberListResponse.java
+++ project/src/main/java/multi/second/project/domain/member/dto/response/MemberListResponse.java
@@ -3,12 +3,17 @@ package multi.second.project.domain.member.dto.response;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import multi.second.project.domain.note.domain.Note;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class MemberListResponse {
+
+    private String userId;
 
 
 
--- project/src/main/java/multi/second/project/domain/note/dto/response/NoteListResponse.java
+++ project/src/main/java/multi/second/project/domain/note/dto/response/NoteListResponse.java
@@ -2,11 +2,18 @@ package multi.second.project.domain.note.dto.response;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import multi.second.project.domain.member.domain.Member;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class NoteListResponse {
 
     private Long ntIdx;
-
+    private List<String> userId;
+    private String lastMassage;
+    private LocalDateTime regDate;
 }
