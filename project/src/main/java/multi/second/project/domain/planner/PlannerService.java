e0bd55f85090284c758123b4662c42eb18f4f7e4
bjh <workbaejh@gmail.com>
2023-04-04 16:53:42 +0900

    test

--- project/src/main/java/multi/second/project/domain/planner/PlannerService.java
+++ project/src/main/java/multi/second/project/domain/planner/PlannerService.java
@@ -221,6 +221,8 @@ public class PlannerService {
 		return member;
 		
 	}
+
+	
 	
 	
 	
--- project/src/main/java/multi/second/project/infra/code/ErrorCode.java
+++ project/src/main/java/multi/second/project/infra/code/ErrorCode.java
@@ -6,6 +6,7 @@ public enum ErrorCode {
 	FAILED_SEND_EMAIL("이메일 전송에 실패했습니다."),
 	UNAUTHORIZED_REQUEST("권한이 없습니다.","/member/login"),
 	HOST_UNAUTHORIZED_REQUEST("권한이 없습니다.","/planner/list"),
+	GROUP_UNAUTHORIZED_REQUEST("권한이 없습니다.","/planner/list"),
 	FAILED_UPLOAD_FILE("파일 업로드 작업 중 에러가 발생했습니다"),
 	NOT_EXISTS("존재하지 않습니다."),
 	FAILED_DELETE_FILE("파일 삭제 작업 중 문제가 발생하였습니다.");
