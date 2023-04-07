b7fdfa579fab1ce1a0e5bd3066021c4767c8b454
bjh <workbaejh@gmail.com>
2023-04-07 14:30:15 +0900

    교통,예산 todo 콤보박스 변경

--- project/src/test/java/multi/second/project/member/MemberControllerTest.java
+++ project/src/test/java/multi/second/project/member/MemberControllerTest.java
@@ -36,7 +36,7 @@ public class MemberControllerTest {
 	public void testSignUpImpl() throws Exception {
 		
 		SignUpRequest form = new SignUpRequest();
-		form.setUserId("group1A");
+		form.setUserId("group1C");
 		form.setPassword("123qwe!@#QWE");
 		form.setEmail("azimemory@gmail.com");
 		form.setGrade("ROLE_USER");
